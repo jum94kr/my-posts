a5a96613ce2e159a01473a1b11f1752d22a88b52
jum94kr <jum94kr@gmail.com>
2023-02-23 01:05:15 +1000

    first draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
+import MyHeader from './MyHeader';
+import MyFooter from './MyFooter';
+import { Button, Form, Input, Select } from 'antd';
+import 'antd/dist/reset.css';
+import { Space, Table, Tag } from 'antd';
+const { Column, ColumnGroup } = Table;
+const { Option } = Select;
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+const data = [
+  {
+    key: '1',
+    firstName: 'John',
+    lastName: 'Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    firstName: 'Jim',
+    lastName: 'Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    firstName: 'Joe',
+    lastName: 'Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+
 
 function App() {
   return (
     <div className="App">
+      <MyHeader/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h2>hdi</h2>
+      <Button type="primary">Button</Button>
+      <Table dataSource={data}>
+    <ColumnGroup title="Name">
+      <Column title="First Name" dataIndex="firstName" key="firstName" />
+      <Column title="Last Name" dataIndex="lastName" key="lastName" />
+    </ColumnGroup>
+    <Column title="Age" dataIndex="age" key="age" />
+    <Column title="Address" dataIndex="address" key="address" />
+    <Column
+      title="Tags"
+      dataIndex="tags"
+      key="tags"
+      render={(tags) => (
+        <>
+          {tags.map((tag) => (
+            <Tag color="blue" key={tag}>
+              {tag}
+            </Tag>
+          ))}
+        </>
+      )}
+    />
+    <Column
+      title="Action"
+      key="action"
+      render={(_, record) => (
+        <Space size="middle">
+          <a>Invite {record.lastName}</a>
+          <a>Delete</a>
+        </Space>
+      )}
+    />
+  </Table>
       </header>
+      <MyFooter/>
     </div>
+
   );
 }
 
---
+++ src/MyHeader.js
@@ -0,0 +1,5 @@
+const MyHeader = () => {
+    return <header>해더</header>
+};
+
+export default MyHeader;
